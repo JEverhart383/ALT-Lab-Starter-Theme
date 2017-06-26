903fac0230039fec9e895b0df4d4155afc25a009
Jeff Everhart <Jeffeverhart383@gmail.com>
2017-06-26 14:05:45 -0400

    update functions.php and add new template pages

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php 
 
+	
+	// Load all of the external CSS and JS
 	function theme_resources(){
 		wp_enqueue_style('bootstrap_css', get_template_directory_uri() . '/css/bootstrap.css'); 
 		wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css'); 
@@ -7,6 +9,29 @@
 	}
 
 	add_action( 'wp_enqueue_scripts', 'theme_resources'); 
+
+	
+
+	function alt_lab_setup(){
+
+		//Add support for custom-logo
+		add_theme_support( 'custom-logo', array(
+			'height'      => 240,
+			'width'       => 240,
+			'flex-height' => true,
+		) );
+
+		add_theme_support('post-thumbnails'); 
+
+
+	    // This theme uses wp_nav_menu() in two locations.
+		register_nav_menus( array(
+			'primary' => __( 'Primary Menu')
+		) );
+
+	}
+
+	add_action('after_setup_theme', 'alt_lab_setup'); 
 
 
 ?>
--- header.php
+++ header.php
@@ -3,7 +3,11 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php bloginfo('name') ?></title>
+	<title>
+	<?php the_title('', ' |', true); ?>
+	<?php bloginfo('name') ?>
+		
+	</title>
 
 <?php wp_head(); ?>
 
@@ -19,15 +23,38 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
+
+          <?php 
+          	$custom_logo_id = get_theme_mod('custom_logo');
+          	$logo = wp_get_attachment_image_src($custom_logo_id, 'full'); 
+          ?>
+
+          <?php if( has_custom_logo()): ?>
+          	<a href="<?php bloginfo('url') ?>"><img class="img img-responsive" src="<?php echo $logo[0] ?>"> </a>
+          <?php else: ?>	
           <a class="navbar-brand" href="<?php bloginfo('url') ?>"><?php bloginfo('name') ?></a>
+      <?php endif;?> 
         </div>
         <div id="navbar" class="navbar-collapse collapse">
         <!-- Print out menu items here -->
+        <?php wp_nav_menu(
+	        array(
+	        'container' => 'ul', 	
+	       	'menu_class' => 'nav navbar-nav', 
+	        'theme_location' => 'primary' 
+	        )
+        );  ?>
 
         </div><!--/.navbar-collapse -->
       </div>
  </nav>
+ <div class="jumbotron">
+	<div class="container">
+		
+	</div>
+</div>
+
  <!-- Begin Main Content Container  -->
- <div class="container">
+<div class="container">
  	
 
